cedc1a7e7f37c8d50ac19f5598e9aaa5eae72dc1
Tom Billington <tombill143@gmail.com>
2023-04-27 10:00:15 +0200

    testing command commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "./Home.module.css";
+import Head from "next/head";
 
 function ContactForm() {
   const [name, setName] = useState("");
@@ -21,48 +22,53 @@ function ContactForm() {
   };
 
   return (
-    <div className="formWrapper">
-      <h1 className={styles.headline}>Find A buyer for your property</h1>
-      <div className={styles.content}>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Price</label>
-          <input
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label htmlFor="email">Size in Meters</label>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label htmlFor="zipcode">Zip Code</label>
-          <input
-            id="zipcode"
-            type="text"
-            value={zipcode}
-            onChange={(e) => setZipcode(e.target.value)}
-          />
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <label htmlFor="property-type">Property Type:</label>
-          <select id="property-type" name="property-type">
-            <option value="">Choose a property type</option>
-            <option value="house">Villa</option>
-            <option value="apartment">Lejlighed</option>
-            <option value="condo">Rækkehus</option>
-          </select>
-          <button type="submit">Submit</button>
-        </form>
+    <>
+      <Head>
+        <title>ContactForm</title>
+      </Head>
+      <div className="formWrapper">
+        <h1 className={styles.headline}>Find A buyer for your property</h1>
+        <div className={styles.content}>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Price</label>
+            <input
+              id="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="email">Size in Square Meters</label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="zipcode">Zip Code</label>
+            <input
+              id="zipcode"
+              type="text"
+              value={zipcode}
+              onChange={(e) => setZipcode(e.target.value)}
+            />
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <label htmlFor="property-type">Property Type:</label>
+            <select id="property-type" name="property-type">
+              <option value="">Choose a property type</option>
+              <option value="house">Villa</option>
+              <option value="apartment">Lejlighed</option>
+              <option value="condo">Rækkehus</option>
+            </select>
+            <button type="submit">Find Potential Buyers</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
