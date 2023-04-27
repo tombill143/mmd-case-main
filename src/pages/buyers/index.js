54df9f9bb1c327c4ce9b35428718b1562a45d207
Your Name <you@example.com>
2023-04-27 14:51:56 +0200

    before i add data stuff and potentially fuck it up

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -2,26 +2,25 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import styles from "./Buyers.module.css";
 
+
 export default function Buyers() {
   const { query } = useRouter();
   return (
     <>
       <Head>
-        <title>Find buyer | EDC</title>
+        <title>Potential buyers | EDC</title>
       </Head>
       <div className="wrapper">
-        <h1 className={styles.headline}>Potential buyers</h1>
+        <h1 className={styles.headline}>Potential buyersss</h1>
+        
         <p>
-          On this page you get the <code>`query`</code> params like{" "}
-          <code>`zipCode`</code>, and can use them to fetch a list of buyers
-          from the API.
-        </p>
-        <p>
-          Make sure to read the docs on how to fetch data on a page - There are
+          Thank you for submitting your info on your property. On this page we have listed suitable buyers. Please read through the list and add the ones that are interesting by clicking the '+' icon in the corner of each buyers. 
+
+          {/* Make sure to read the docs on how to fetch data on a page - There are
           multiple ways of doing it, and you should choose the one that fits
-          your solution best.
+          your solution best. */}
         </p>
-        <ul>
+        {/* <ul>
           <li>
             <a
               href="https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props"
@@ -38,13 +37,41 @@ export default function Buyers() {
               react.dev - Fetching data
             </a>
           </li>
-        </ul>
-        <div className={styles.content}>
-          <h2>Query params:</h2>
-          <pre>
-            <code>{JSON.stringify(query, null, 2)}</code>
-          </pre>
+        </ul> */}
+
+        <div className={styles.content_container}>
+           <div className={styles.content}>
+             {/* <h2>Query params:</h2> */}
+             <h2>Buyer X:</h2>
+             <pre>
+             <code>{JSON.stringify(query, null, 2)}</code>
+              </pre>
+           </div>
+
+           <div className={styles.content}>
+             <h2>Buyer X:</h2>
+             <pre>
+             <code>{JSON.stringify(query, null, 2)}</code>
+             </pre>
+           </div>
         </div>
+        <div className={styles.content_container}>
+           <div className={styles.content}>
+             {/* <h2>Query params:</h2> */}
+             <h2>Buyer X:</h2>
+             <pre>
+             <code>{JSON.stringify(query, null, 2)}</code>
+              </pre>
+           </div>
+
+           <div className={styles.content}>
+             <h2>Buyer X:</h2>
+             <pre>
+             <code>{JSON.stringify(query, null, 2)}</code>
+             </pre>
+           </div>
+        </div>
+
       </div>
     </>
   );
