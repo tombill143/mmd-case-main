f4f4deda0fb767898005db8b90f99b3a40207e9e
Monika Gorgics <moni6592@stud.kea.dk>
2023-04-30 15:16:56 +0200

    frame

---
+++ src/pages/dashboard/dashboard.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import styles from "./dashboard.module.css";
+
+function App () {
+<>
+<Head>
+        <title>Admin Dashboard | EDC</title>
+      </Head>
+<article>
+<p>Date</p>
+<p>Name</p>
+<p>Email</p>
+<p>Phone</p>
+<p>Contact</p>
+<p>Price</p>
+<p>Zipcode</p>
+<p>Size</p>
+<p>Estate type</p>
+<button>Delete</button>
+</article>
+</>
+    return(
+
+    );
+}
