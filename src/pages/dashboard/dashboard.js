f5c226094ca9fd1d7e8934caf3b489de2951d063
Monika Gorgics <moni6592@stud.kea.dk>
2023-04-30 15:36:19 +0200

    links

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,28 +1,31 @@
 import { useState } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import styles from "./dashboard.module.css";
+import "@/styles/globals.css";
+import Link from "next/link";
 
-function AdminDashBoard () {
-<>
-  <Head>
-        <title>Admin Dashboard | EDC</title>
-  </Head>
-<article>
-<p>Date</p>
-<p>Name</p>
-<p>Email</p>
-<p>Phone</p>
-<p>Contact</p>
-<p>Price</p>
-<p>Zipcode</p>
-<p>Size</p>
-<p>Estate type</p>
-<button>Delete</button>
-</article>
-</>
-    return(
-
-    );
+function AdminDashBoard() {
+  <>
+    <Head>
+      <title>Admin Dashboard | EDC</title>
+    </Head>
+    <article>
+      <p>Date</p>
+      <p>Name</p>
+      <Link to>
+        {" "}
+        <p>Email</p>{" "}
+      </Link>
+      <Link to>
+        <p>Phone</p>{" "}
+      </Link>
+      <p>Contact</p>
+      <p>Price</p>
+      <p>Zipcode</p>
+      <p>Size</p>
+      <p>Estate type</p>
+      <button>Delete</button>
+    </article>
+  </>;
+  return {};
 }
 export default AdminDashBoard;
