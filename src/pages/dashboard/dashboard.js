02d7b0765a6c580246a067035ef437cf052abd1a
Monika Gorgics <moni6592@stud.kea.dk>
2023-05-01 11:12:16 +0200

    confused

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,32 +1,41 @@
 import { useState } from "react";
 import Head from "next/head";
-import "@/styles/globals.css";
-import Link from "next/link";
+
+//import supabase
 
 function AdminDashBoard() {
-  <>
-    <Head>
-      <title>Admin Dashboard | EDC</title>
-    </Head>
-    <article>
-      <h1 className={styles.headline}>Latest Contacts</h1>
-      <p>Date</p>
-      <p>Name</p>
-      <Link to>
-        {" "}
-        <p>Email</p>{" "}
-      </Link>
-      <Link to>
-        <p>Phone</p>{" "}
-      </Link>
-      <p>Contact</p>
-      <p>Price</p>
-      <p>Zipcode</p>
-      <p>Size</p>
-      <p>Estate type</p>
-      <button>Delete</button>
-    </article>
-  </>;
-  return {};
+  //const [date, setDate] = import from Supabase (sort by create at)
+  //const [name, setName] =  import data from Supabase (id?)
+  //const [email, setEmail] = useState("");
+  //prize fetch data from api
+  //const [zipcode, setZipcode] = useState(""); fetch data from api
+  //size from api
+  //estate type fetch data from api
+
+  return (
+    <>
+      <Head>
+        <title>Admin Dashboard | EDC</title>
+      </Head>
+      <div className="AdDashWrapper">
+        <h1 className={styles.headline}>Latest Contacts</h1>
+        <p>Date</p>
+        <p>Name</p>
+        <Link to>
+          {" "}
+          <p>Email</p>{" "}
+        </Link>
+        <Link to>
+          <p>Phone</p>{" "}
+        </Link>
+        <p>Contact</p>
+        <p>Price</p>
+        <p>Zipcode</p>
+        <p>Size</p>
+        <p>Estate type</p>
+        <button className={styles.button}>X</button>
+      </div>
+    </>
+  );
 }
 export default AdminDashBoard;
