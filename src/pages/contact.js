d016863cfa6499473b119134529123aac9374ee9
Tom Billington <tombill143@gmail.com>
2023-05-01 11:30:25 +0200

    contact form done for now. need supabase stuff now

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import styles from "./buyers/Buyers.module.css";
 
 export default function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [zipcode, setZipcode] = useState("");
+  const [phone, setPhone] = useState("");
   const [selectedBuyers, setSelectedBuyers] = useState([]);
 
   const router = useRouter();
@@ -22,15 +22,17 @@ export default function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Do something with the form data and the selectedBuyers data
-    console.log({ name, email, message, zipcode, selectedBuyers });
+    console.log({ name, email, phone, selectedBuyers });
   };
 
   return (
     <>
-      <h1>Contact Potential Buyers</h1>
+      <h1 className={styles.headline}>Contact Potential Buyers</h1>
       {selectedBuyers.length > 0 && (
         <>
-          <h2>Selected buyers:</h2>
+          <h2 id="selectedBuyers" className={styles.headline}>
+            Selected buyers:
+          </h2>
           <ul>
             {selectedBuyers.map((buyer) => (
               <li key={buyer}>Buyer Id: {buyer}</li>
@@ -38,10 +40,41 @@ export default function Contact() {
           </ul>
         </>
       )}
-      <form onSubmit={handleSubmit}>
-        {/* Form fields */}
-        <button type="submit">Submit</button>
-      </form>
+      <div id="contactform" className="wrapper">
+        <div className={styles.content}>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Phone:
+              <input
+                type="tel"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                maxLength={8}
+                required
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
